87e4c542ff0822ec55b6365888822563eaa4831b
Nuttaphon Ratsameeraweemat <nuttaphon.rat@hotmail.com>
2019-05-29 14:39:14 +0700

    Update -Required Fields in project view model

--- TimeSheet.Bll/Models/ProjectViewModel.cs
+++ TimeSheet.Bll/Models/ProjectViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TimeSheet.Bll.Models
@@ -7,8 +8,11 @@ namespace TimeSheet.Bll.Models
     public class ProjectViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string ProjectCode { get; set; }
+        [Required]
         public string ProjectName { get; set; }
+        [Required]
         public string Status { get; set; }
     }
 }
