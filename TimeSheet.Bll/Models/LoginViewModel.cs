552f869026bb9f0ef0c6887f74839c2bfe9c62c1
Nuttaphon Ratsameeraweemat <nuttaphon.rat@hotmail.com>
2019-05-30 13:04:10 +0700

    Update -Login ViewMdeol DataAnnotations

--- TimeSheet.Bll/Models/LoginViewModel.cs
+++ TimeSheet.Bll/Models/LoginViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TimeSheet.Bll.Models
 {
     public class LoginViewModel
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
